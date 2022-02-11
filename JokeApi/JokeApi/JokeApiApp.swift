f3889b4e3747f310555cbfe32e1471390c182027
dragonscry <denismerkotun@gmail.com>
2022-02-11 17:57:54 +0200

    add jokeapi template

---
+++ JokeApi/JokeApi/JokeApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeApiApp.swift
+//  JokeApi
+//
+//  Created by Denys on 11.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct JokeApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
