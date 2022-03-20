acc25deecaca6603d323a1c794375ca0a706a16f
dragonscry <denismerkotun@gmail.com>
2022-03-21 00:01:09 +0200

    upd for text

--- BootcampCourse/BootcampCourse/TextBootcamp.swift
+++ BootcampCourse/BootcampCourse/TextBootcamp.swift
@@ -14,9 +14,28 @@ struct TextBootcamp: View {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 .font(.title)
             //Bold Text + underline
-            Text("Bold")
+            Text("Bold text".capitalized)
                 .fontWeight(.heavy)
                 .underline()
+            // Italic + strike
+            Text("Im Italic")
+                .italic()
+                .strikethrough()
+            //Underline with color
+            Text("Underline Color".lowercased())
+                .font(.title)
+                .underline(true, color: Color.green)
+            //Text through systeb
+            Text("System")
+                .font(.system(size: 24, weight: .semibold, design: .serif))
+            //Text Multiline
+            Text("Multiline Text! This is Text for course and for practicing.")
+                .baselineOffset(10) // baseline offset between text
+                .kerning(5) // offset between letters
+                .multilineTextAlignment(.center) // how will be multiline text oriented
+                .foregroundColor(.blue) //text color
+                .frame(width: 200, height: 100, alignment: .leading) // frame for text
+                .minimumScaleFactor(0.1) //resize for text
         }
     }
 }
