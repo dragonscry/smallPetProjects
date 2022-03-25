3a75da79450789a7c2a68c88fe7934cb2e3830a8
dragonscry <denismerkotun@gmail.com>
2022-03-26 00:06:48 +0200

    frame lesson upd

--- BootcampCourse/BootcampCourse/FrameBootcamp.swift
+++ BootcampCourse/BootcampCourse/FrameBootcamp.swift
@@ -10,6 +10,20 @@ import SwiftUI
 struct FrameBootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(Color.red)
+            .frame(height: 100, alignment: .top)
+            //.frame(width: .infinity, height: 200, alignment: .leading)
+            //.background(Color.red)
+            //.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .background(Color.orange)
+            .frame(width: 150)
+            .background(Color.purple)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.pink)
+            .frame(height: 400)
+            .background(Color.green)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(Color.yellow)
     }
 }
 
