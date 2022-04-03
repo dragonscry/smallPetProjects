7f7787b1ae999c59e016dcd8187781b87dd467d8
dragonscry <denismerkotun@gmail.com>
2022-04-03 23:49:54 +0300

    lazy v grid lesson

--- BootcampCourse/BootcampCourse/LazyVGridBootcamp.swift
+++ BootcampCourse/BootcampCourse/LazyVGridBootcamp.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct LazyVGridBootcamp: View {
+    
+    let columns: [GridItem] = [
+      //  GridItem(.fixed(50), spacing: nil, alignment: nil)
+       // GridItem(.flexible(), spacing: nil, alignment: nil)
+      //  GridItem(.adaptive(minimum: 10, maximum: 300), spacing: nil, alignment: nil)
+        GridItem(.flexible(), spacing: 10), //this spacing between COLUMNS
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text("Ji")
+        ScrollView {
+            
+            Rectangle()
+                .fill(Color.orange)
+                .frame(height: 400)
+            
+            LazyVGrid(columns: columns,
+                      alignment: .center,
+                      spacing: 20, //this spacing between ROADS
+                      pinnedViews: [.sectionHeaders]) {
+                
+                Section(header:
+                            Text("Section one")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color.blue)
+                    .padding()
+                ){
+                    ForEach(0..<20) { index in
+                        Rectangle()
+                            .frame(height: 150)
+                    }
+                }
+                
+                Section(header:
+                            Text("Section two")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color.red)
+                    .padding()
+                ){
+                    ForEach(0..<20) { index in
+                        Rectangle()
+                            .fill(Color.green)
+                            .frame(height: 150)
+                    }
+                }
+            }
+            
+//            Rectangle()
+//                .fill(Color.white)
+//                .frame(height: 400)
+            
+//            LazyVGrid(columns: columns) {
+//                ForEach(0..<50) { index in
+//                    Rectangle().frame(height: 150)
+//                }
+//            }
+        }
     }
 }
 
