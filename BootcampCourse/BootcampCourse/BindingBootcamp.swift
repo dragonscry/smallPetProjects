6e456e3e5c6696122c73bea61087a3c9c737fbc2
dragonscry <denismerkotun@gmail.com>
2022-04-08 23:03:25 +0300

    binding lesson

---
+++ BootcampCourse/BootcampCourse/BindingBootcamp.swift
@@ -0,0 +1,56 @@
+//
+//  BindingBootcamp.swift
+//  BootcampCourse
+//
+//  Created by Denys on 08.04.2022.
+//
+
+import SwiftUI
+
+struct BindingBootcamp: View {
+    
+    @State var backgroundColor: Color = Color.green
+    @State var title = "title"
+    
+    var body: some View {
+        ZStack {
+            backgroundColor
+                .ignoresSafeArea()
+            
+            VStack {
+                Text(title)
+                    .foregroundColor(.white)
+                
+                ButtonView(backgroundColor: $backgroundColor, title: $title)
+            }
+        }
+    }
+}
+
+struct BindingBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingBootcamp()
+    }
+}
+
+struct ButtonView: View {
+    
+    @Binding var backgroundColor: Color
+    @State var buttonColor: Color = Color.blue
+    @Binding var title: String
+    
+    var body: some View {
+        Button(action: {
+            backgroundColor = .orange
+            buttonColor = .red
+            title = "PRESSED TITLE"
+        }, label: {
+            Text("Button")
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(buttonColor)
+                .cornerRadius(10)
+        })
+    }
+}
