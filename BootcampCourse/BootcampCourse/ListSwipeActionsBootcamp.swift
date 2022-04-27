4a9ceee07f2651a18f04fb54de96a0c970f6d481
dragonscry <denismerkotun@gmail.com>
2022-04-27 23:07:55 +0300

    swipe actions lesson

---
+++ BootcampCourse/BootcampCourse/ListSwipeActionsBootcamp.swift
@@ -0,0 +1,52 @@
+//
+//  ListSwipeActionsBootcamp.swift
+//  BootcampCourse
+//
+//  Created by Denys on 27.04.2022.
+//
+
+import SwiftUI
+
+struct ListSwipeActionsBootcamp: View {
+    
+    @State var fruits: [String] = ["apple","orange", "banana", "peach"]
+    
+    var body: some View {
+        List{
+            ForEach(fruits, id: \.self) {
+                Text($0.capitalized)
+                    .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                        Button("Archive") {
+                            
+                        }
+                        .tint(.green)
+                        Button("Save") {
+                            
+                        }
+                        .tint(.orange)
+                        Button("Junk") {
+                            
+                        }
+                        .tint(.blue)
+                    }
+                    .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                        Button("Share") {
+                            
+                        }
+                        .tint(.yellow)
+                    }
+            }
+            //.onDelete(perform: delete)
+        }
+    }
+    
+    func delete(indexSet: IndexSet){
+        
+    }
+}
+
+struct ListSwipeActionsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ListSwipeActionsBootcamp()
+    }
+}
