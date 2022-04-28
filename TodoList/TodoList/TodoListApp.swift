abf4573b574de81ac44196817dd0a2d349b57ed8
dragonscry <denismerkotun@gmail.com>
2022-04-28 12:36:17 +0300

    new app. list view

---
+++ TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Denys on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoList/TodoList/View/ListRowView.swift
@@ -0,0 +1,20 @@
+//
+//  ListRowView.swift
+//  TodoList
+//
+//  Created by Denys on 28.04.2022.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListRowView()
+    }
+}
