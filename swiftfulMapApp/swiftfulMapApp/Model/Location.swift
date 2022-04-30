030b61c0d2414f9eb6cf18a523fc87d1a6ae6422
dragonscry <denismerkotun@gmail.com>
2022-04-30 23:45:31 +0300

    add setup for app

---
+++ swiftfulMapApp/swiftfulMapApp/Model/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  swiftfulMapApp
+//
+//  Created by Denys on 30.04.2022.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name : String
+    let cityName : String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
---
+++ swiftfulMapApp/swiftfulMapApp/swiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftfulMapAppApp.swift
+//  swiftfulMapApp
+//
+//  Created by Denys on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
