665b19be6efa49472da625c4ffe0d0226f4882bf
dragonscry <denismerkotun@gmail.com>
2022-05-04 12:20:13 +0300

    custom pins

--- swiftfulMapApp/swiftfulMapApp/View/LocationVIew.swift
+++ swiftfulMapApp/swiftfulMapApp/View/LocationVIew.swift
@@ -16,25 +16,14 @@ struct LocationsView: View {
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $vm.mapRegion)
+            mapLayer
                 .ignoresSafeArea()
-            
             VStack(spacing: 0) {
-               header
+                header
                     .padding()
-                
                 Spacer()
-                
-                ZStack {
-                    ForEach(vm.locations) { location in
-                        if vm.mapLocation == location {
-                            LocationPreviewView(location: location)
-                                .shadow(color: .black.opacity(0.3), radius: 20)
-                                .padding()
-                                .transition(.asymmetric(insertion: .move(edge:.trailing), removal: .move(edge: .leading)))
-                        }
-                    }
-                }
+                locationsPreviewStack
+
             }
         }
     }
@@ -62,7 +51,7 @@ extension LocationsView {
                     .frame(height: 55)
                     .frame(maxWidth: .infinity)
                     .animation(.none, value: vm.mapLocation)
-                    //.background(Color.red)
+                //.background(Color.red)
                     .overlay(alignment: .leading) {
                         Image(systemName: "arrow.down")
                             .font(.headline)
@@ -71,7 +60,7 @@ extension LocationsView {
                             .rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0))
                     }
             }
-
+            
             
             
             if vm.showLocationsList {
@@ -84,5 +73,33 @@ extension LocationsView {
         .background(.thickMaterial)
         .cornerRadius(10)
         .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
+    private var mapLayer: some View {
+        //           Map(coordinateRegion: $vm.mapRegion)
+        Map(coordinateRegion: $vm.mapRegion, annotationItems: vm.locations, annotationContent: { location in
+            //             MapMarker(coordinate: location.coordinates, tint: .blue)
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: .black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(insertion: .move(edge:.trailing), removal: .move(edge: .leading)))
+                }
+            }
+        }
     }
 }
