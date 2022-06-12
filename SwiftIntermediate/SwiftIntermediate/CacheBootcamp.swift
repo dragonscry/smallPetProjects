c1ccf2487be070bcdc0caf32d9244502b50cf6fa
dragonscry <denismerkotun@gmail.com>
2022-06-12 21:31:59 +0300

    cache lesson

---
+++ SwiftIntermediate/SwiftIntermediate/CacheBootcamp.swift
@@ -0,0 +1,157 @@
+//
+//  CacheBootcamp.swift
+//  SwiftIntermediate
+//
+//  Created by Denys on 12.06.2022.
+//
+
+class CacheManager {
+    
+    static let instance = CacheManager() //Singleton
+    private init() {
+        
+    }
+    
+    var imageCache: NSCache<NSString, UIImage> = {
+        let cache = NSCache<NSString, UIImage>()
+        cache.countLimit = 100
+        cache.totalCostLimit = 1024 * 1024 * 100 //100mb
+        return cache
+    }()
+    
+    func add(image: UIImage, name: String) -> String {
+        imageCache.setObject(image, forKey: name as NSString)
+        return "Added to cache!"
+    }
+    
+    func remove(name: String) -> String {
+        imageCache.removeObject(forKey: name as NSString)
+        return "Removed from cache!"
+    }
+    
+    func get(name:String) -> UIImage? {
+        return imageCache.object(forKey: name as NSString)
+    }
+}
+
+class CacheViewModel : ObservableObject {
+    @Published var startingImage: UIImage? = nil
+    @Published var cachedImage: UIImage? = nil
+    @Published var infoMessage: String = ""
+    
+    let imageName: String = "horizon"
+    let manager = CacheManager.instance
+    
+    
+    init() {
+        getImageFromAssetsFolder()
+    }
+    
+    func getImageFromAssetsFolder() {
+        startingImage = UIImage(named: imageName)
+    }
+    
+    func saveToCache() {
+        guard let image = startingImage else {return}
+        infoMessage = manager.add(image: image, name: imageName)
+    }
+    
+    func removeFromCache() {
+        infoMessage = manager.remove(name: imageName)
+    }
+    
+    func getFromCache() {
+        
+        if let returnedImage = manager.get(name: imageName) {
+            cachedImage = returnedImage
+            infoMessage = "Got image from cache"
+        } else {
+            infoMessage = "Image not found in cache"
+        }
+    }
+}
+
+import SwiftUI
+import Combine
+
+struct CacheBootcamp: View {
+    
+    @StateObject var vm = CacheViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                if let image = vm.startingImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .cornerRadius(10)
+                }
+                
+                Text(vm.infoMessage)
+                    .font(.headline)
+                    .foregroundColor(.purple)
+                
+                HStack {
+                    Button {
+                        vm.saveToCache()
+                    } label: {
+                        Text("Save to cache")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.blue)
+                            .cornerRadius(10)
+                    }
+                    
+                    
+                    Button {
+                        vm.removeFromCache()
+                    } label: {
+                        Text("Delete from cache")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(.red)
+                            .cornerRadius(10)
+                    }
+                    
+                }
+                
+                Button {
+                    vm.getFromCache()
+                } label: {
+                    Text("Get from cache")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.green)
+                        .cornerRadius(10)
+                }
+                
+                if let image = vm.cachedImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .cornerRadius(10)
+                }
+                
+                
+                Spacer()
+                
+                
+            }
+            .navigationTitle("Cache Bootcamp")
+        }
+    }
+}
+
+struct CacheBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        CacheBootcamp()
+    }
+}
