4cfb72b12fcac3d0b3687239b7157cb90545d0ca
dragonscry <denismerkotun@gmail.com>
2022-06-21 21:49:27 +0300

    add cache to proj

---
+++ SwiftIntermediate/SwiftIntermediate/DownloadingImages/Utilities/PhotoModelFileManager.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoModelFileManager.swift
+//  SwiftIntermediate
+//
+//  Created by Denys on 21.06.2022.
+//
+
+import Foundation
+import SwiftUI
+
+class PhotoModelFileManager {
+    
+    static let instance = PhotoModelFileManager()
+    
+    private init() {
+        createFolderIfNeeded()
+    }
+    
+    private func createFolderIfNeeded() {
+        
+    }
+    
+    private func getFolderPath() {
+        
+    }
+    
+}
