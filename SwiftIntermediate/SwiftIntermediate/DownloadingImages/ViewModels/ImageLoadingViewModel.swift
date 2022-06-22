e58c8e172daf1bb631a8e6241033d4d7ea30cce3
dragonscry <denismerkotun@gmail.com>
2022-06-22 11:51:16 +0300

    end of proj and intermediate course

--- SwiftIntermediate/SwiftIntermediate/DownloadingImages/ViewModels/ImageLoadingViewModel.swift
+++ SwiftIntermediate/SwiftIntermediate/DownloadingImages/ViewModels/ImageLoadingViewModel.swift
@@ -15,12 +15,25 @@ class ImageLoadingViewModel: ObservableObject {
     @Published var isLoading: Bool = false
     
     var cancellables = Set<AnyCancellable>()
+    let manager = PhotoModelCacheManager.instance
     
     let urlString: String
+    let imageKey: String
     
-    init(url: String) {
+    init(url: String, key: String) {
         urlString = url
-        downloadImage()
+        imageKey = key
+        getImage()
+    }
+    
+    func getImage() {
+        if let savedImage = manager.get(key: imageKey) {
+            image = savedImage
+            print("Getting saved image")
+        } else {
+            downloadImage()
+            print("Downloading image now")
+        }
     }
     
     func downloadImage() {
@@ -38,7 +51,10 @@ class ImageLoadingViewModel: ObservableObject {
             .sink { [weak self] (_) in
                 self?.isLoading = false
             } receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
+                guard let self = self,
+                      let image = returnedImage else {return}
+                self.image = image
+                self.manager.add(key: self.imageKey, value: image)
             }
             .store(in: &cancellables)
 
--- SwiftIntermediate/SwiftIntermediate/DownloadingImages/Views/DownloadingImageView.swift
+++ SwiftIntermediate/SwiftIntermediate/DownloadingImages/Views/DownloadingImageView.swift
@@ -11,8 +11,8 @@ struct DownloadingImageView: View {
     
     @StateObject var loader: ImageLoadingViewModel
     
-    init(url: String) {
-        _loader = StateObject(wrappedValue: ImageLoadingViewModel(url: url))
+    init(url: String, key: String) {
+        _loader = StateObject(wrappedValue: ImageLoadingViewModel(url: url, key: key))
     }
     
     var body: some View {
