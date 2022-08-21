1a2e1750426579013a44e8ddabe1902efced4612
dragonscry <denismerkotun@gmail.com>
2022-08-21 13:05:48 +0300

    add market data model

--- SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewsModels/HomeViewModel.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Home/ViewsModels/HomeViewModel.swift
@@ -10,20 +10,16 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
-    @Published var statistics: [StatisticModel] = [
-        StatisticModel(title: "Title", value: "Value", percentageChange: 1),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value", percentageChange: -7)
-    ]
+    @Published var statistics: [StatisticModel] = []
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
     @Published var searchText: String = ""
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
     private var cansellables = Set<AnyCancellable>()
+    private let marketDataService = MarketDataService()
     
     init() {
         addSubscribers()
@@ -33,11 +29,18 @@ class HomeViewModel: ObservableObject {
         
         //updates allCoins
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
+            }
+            .store(in: &cansellables)
+        
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
             }
             .store(in: &cansellables)
     }
@@ -54,5 +57,26 @@ class HomeViewModel: ObservableObject {
             coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
         }
+    }
+    
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+        guard let data = marketDataModel else {
+            return stats
+        }
+        
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        let volume = StatisticModel(title: "24h Volume", value: data.volume)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0.0)
+        
+        stats.append(contentsOf: [
+            marketCap,
+            volume,
+            btcDominance,
+            portfolio
+        ])
+        
+        return stats
     }
 }
---
+++ SwiftfulCrypto/SwiftfulCrypto/Services/MarketDataService.swift
@@ -0,0 +1,38 @@
+//
+//  MarketDataService.swift
+//  SwiftfulCrypto
+//
+//  Created by Denys on 21.08.2022.
+//
+
+import Foundation
+import Combine
+
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    
+    //var cancellables = Set<AnyCancellable>()
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else {return}
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+            })
+
+        
+    }
+    
+}
+
