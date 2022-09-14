4430903e905df502f4fc81d86d4f5fef3e70f1e6
dragonscry <denismerkotun@gmail.com>
2022-09-15 00:05:41 +0300

    add graph to app

--- SwiftfulCrypto/SwiftfulCrypto/Core/Detail/Views/DetailView.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Detail/Views/DetailView.swift
@@ -35,33 +35,54 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
-                Text("")
-                    .frame(height: 150)
+            
+            VStack{
+                ChartView(coin: vm.coin)
+                    .padding(.vertical)
                 
-                overviewTitle
-                Divider()
-                overviewGrid
-                
-                additionalTitle
-                Divider()
-                additionalGrid
+                VStack(spacing: 20) {
+                    
+                    overviewTitle
+                    Divider()
+                    overviewGrid
+                    
+                    additionalTitle
+                    Divider()
+                    additionalGrid
+                }
+                .padding()
             }
-            .padding()
+            
         }
         .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            DetailView(coin: dev.coin)
-        }
+            NavigationView {
+                DetailView(coin: dev.coin)
+            }
+            .preferredColorScheme(.dark)
     }
 }
 
 extension DetailView {
+    
+    private var navigationBarTrailingItems: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+            .foregroundColor(Color.theme.secondaryText)
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
     
     private var overviewTitle: some View {
         Text("Overview")
