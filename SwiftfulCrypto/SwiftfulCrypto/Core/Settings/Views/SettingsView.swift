5d4bae5d36721b72b023274a493e2dd10b5bda99
dragonscry <denismerkotun@gmail.com>
2022-09-15 23:23:56 +0300

    add settings view with info

---
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,122 @@
+//
+//  SettingsView.swift
+//  SwiftfulCrypto
+//
+//  Created by Denys on 15.09.2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    let defaultURL = URL(string: "https://www.google.com")!
+    let youtubeURL = URL(string: "https://www.youtube.com/c/swiftulthinking")!
+    let coffeURL = URL(string: "https://www.buymeacoffe.com/nicksarno")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    let personalURL = URL(string: "https://www.nicksarno.com")!
+    
+    var body: some View {
+        NavigationView {
+            List {
+                SwiftullThinkingSection
+                CoinGeckoSection
+                DeveloperSection
+                applicationSection
+
+            }
+            .tint(.blue)
+            .font(.headline)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        XMarkButton()
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
+extension SettingsView {
+    private var SwiftullThinkingSection: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was made by following a @Swiftullthinking course on YouTube. It Uses MVVM Architecture, Combine and CoreData!")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Subscribe on YouTube 🥹", destination: youtubeURL)
+            Link("Support his coffe addiction ☕️", destination: coffeURL)
+        } header: {
+            Text("Swiftful Thinking")
+        }
+    }
+    
+    private var CoinGeckoSection: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("The cryprocurrency data that is used in this app comes from a free API from CoinGecko! Prices may be slighty delayed.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit CoinGecko 🥹", destination: coingeckoURL)
+        } header: {
+            Text("CoinGecko")
+        }
+    }
+    
+    private var DeveloperSection: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was developed by Nick Sarno. It uses SwiftUI and is written 100% in Swift. The project benefits from multi-threading, publishers/subscribers, and data persistance.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit Website 🥹", destination: personalURL)
+        } header: {
+            Text("Developer")
+        }
+    }
+    
+    private var applicationSection: some View {
+        Section {
+            Link("Terms of Service", destination: defaultURL)
+            Link("Privacy Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
+        } header: {
+            Text("Application")
+        }
+    }
+}
